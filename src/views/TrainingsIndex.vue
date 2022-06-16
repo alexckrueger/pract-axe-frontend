16d39ee4a5a31a712ca57d1c2e3e2b06e698304a
Alex Krueger <alexckrueger@gmail.com>
2022-06-16 15:01:05 -0500

    adds table to trainings index

--- src/views/TrainingsIndex.vue
+++ src/views/TrainingsIndex.vue
@@ -26,8 +26,61 @@ export default {
 
 <template>
   <div class="TrainingsIndex">
-    <p>{{ trainings }}</p>
+    <h1>My Trainings</h1>
+    <table>
+      <tr>
+        <th>Training</th>
+        <th>Avg. Axe</th>
+        <th>Hatchets</th>
+        <th>H Avg.</th>
+        <th>H Bullseye Accuracy</th>
+        <th>H Clutch Accuracy</th>
+        <th>Big Axes</th>
+        <th>BA Avg.</th>
+        <th>BA Bullseye Accuracy</th>
+        <th>BA Clutch Accuracy</th>
+      </tr>
+      <tr v-for="training in trainings" v-bind:key="training.id">
+        <router-link :to="`/trainings/${training.id}/edit`">
+          <td class="clickable">{{ training.name }}</td>
+        </router-link>
+        <td>{{ training.average_axe }}</td>
+        <td v-if="training.hatchet_throws.throw_count > 0">{{ training.hatchet_throws.throw_count }}</td>
+        <td v-else>-</td>
+        <td v-if="training.hatchet_throws.throw_count > 0">{{ training.hatchet_throws.average_axe }}</td>
+        <td v-else>-</td>
+        <td v-if="training.hatchet_throws.bullseye_accuracy > 0">{{ training.hatchet_throws.bullseye_accuracy }}%</td>
+        <td v-else>-</td>
+        <td v-if="training.hatchet_throws.clutch_accuracy > 0">{{ training.hatchet_throws.clutch_accuracy }}%</td>
+        <td v-else>-</td>
+        <td v-if="training.big_axe_throws.throw_count > 0">{{ training.big_axe_throws.throw_count }}</td>
+        <td v-else>-</td>
+        <td v-if="training.big_axe_throws.throw_count > 0">{{ training.big_axe_throws.average_axe }}</td>
+        <td v-else>-</td>
+        <td v-if="training.big_axe_throws.bullseye_accuracy > 0">{{ training.big_axe_throws.bullseye_accuracy }}%</td>
+        <td v-else>-</td>
+        <td v-if="training.big_axe_throws.clutch_accuracy > 0">{{ training.big_axe_throws.clutch_accuracy }}%</td>
+        <td v-else>-</td>
+      </tr>
+    </table>
   </div>
 </template>
 
-<style></style>
+<style>
+th {
+  padding-right: 1em;
+  padding-left: 1em;
+  border-bottom-style: outset;
+  border-right-style: solid;
+  border-width: 1px;
+}
+td {
+  padding-right: 1em;
+  border-right-style: solid;
+  border-bottom-style: solid;
+  border-width: 1px;
+}
+.clickable:hover {
+  background-color: yellow;
+}
+</style>
