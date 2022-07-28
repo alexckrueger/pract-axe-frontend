603cbfb987a1759f82328edb1e6772a24bb6489d
Alex Krueger <alexckrueger@gmail.com>
2022-07-28 11:01:02 -0500

    show data on trainings edit

--- src/views/TrainingsEdit.vue
+++ src/views/TrainingsEdit.vue
@@ -1,19 +1,121 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Trainings Edit",
+      training: {},
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    axios.get(`/trainings/${this.$route.params.id}.json`).then((response) => {
+      this.training = response.data;
+      console.log(response.data);
+    });
+  },
+  methods: {
+    throwTypeName: function (axeThrow) {
+      if (axeThrow.big_axe) {
+        return "Big Axe";
+      } else {
+        return "Hatchet";
+      }
+    },
+    clutchCalledName: function (axeThrow) {
+      if (axeThrow.clutch_called) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    },
+  },
 };
 </script>
 
 <template>
   <div class="TrainingsEdit">
-    <h1>{{ message }}</h1>
+    <h1>{{ training.name }}</h1>
+    <!-- <button>Update Training Name</button> -->
+    <!-- Figure out how I want users to be able to change the training name -->
+    <p>Average score per axe: {{ training.average_axe }}</p>
+    <div v-if="training.hatchet_throws.throw_count > 0">
+      <p>Hatchet throws: {{ training.hatchet_throws.throw_count }}</p>
+    </div>
+    <div v-if="training.big_axe_throws.throw_count > 0">
+      <p>Big Axe throws: {{ training.big_axe_throws.throw_count }}</p>
+    </div>
+
+    <!-- Move this to TrainingsShow -->
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Hatchet</th>
+          <th>Big Axe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Throw Count</td>
+          <td>{{ training.hatchet_throws.throw_count }}</td>
+          <td>{{ training.big_axe_throws.throw_count }}</td>
+        </tr>
+        <tr>
+          <td>Average Axe</td>
+          <td v-if="training.hatchet_throws.throw_count > 0">{{ training.hatchet_throws.average_axe }}</td>
+          <td v-else>-</td>
+          <td v-if="training.big_axe_throws.throw_count > 0">{{ training.big_axe_throws.average_axe }}</td>
+          <td v-else>-</td>
+        </tr>
+        <tr>
+          <td>Bullseyes</td>
+          <td v-if="training.hatchet_throws.bullseye_accuracy > 0">{{ training.hatchet_throws.bullseye_accuracy }}%</td>
+          <td v-else>-</td>
+          <td v-if="training.big_axe_throws.bullseye_accuracy > 0">{{ training.big_axe_throws.bullseye_accuracy }}%</td>
+          <td v-else>-</td>
+        </tr>
+        <tr>
+          <td>Clutches</td>
+          <td v-if="training.hatchet_throws.clutch_accuracy > 0">{{ training.hatchet_throws.clutch_accuracy }}%</td>
+          <td v-else>-</td>
+          <td v-if="training.big_axe_throws.clutch_accuracy > 0">{{ training.big_axe_throws.clutch_accuracy }}%</td>
+          <td v-else>-</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <br />
+
+    <table>
+      <thead>
+        <tr>
+          <th>Throw</th>
+          <th>Points</th>
+          <th>Throw type</th>
+          <th>Clutch called</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="axeThrow in training.throws" v-bind:key="axeThrow.id">
+          <td class="count"></td>
+          <td>
+            <b>{{ axeThrow.points }}</b>
+          </td>
+          <td>{{ throwTypeName(axeThrow) }}</td>
+          <td>{{ clutchCalledName(axeThrow) }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<style></style>
+<style>
+table {
+  counter-reset: section;
+}
+
+.count:before {
+  counter-increment: section;
+  content: counter(section);
+}
+</style>
