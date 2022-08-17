5a2a9853f81564c9d6e44f5fcc19b0e753ad3790
Alex Krueger <alexckrueger@gmail.com>
2022-08-17 15:25:23 -0500

    styles user show

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -25,34 +25,60 @@ export default {
 </script>
 
 <template>
-  <div class="UsersShow">
-    <h1>{{ user.name }}</h1>
-    <p>{{ user.email }}</p>
-    <router-link to="/trainings">My Trainings</router-link>
-    <div v-if="user.count_of_trainings > 0">
-      <h2>My stats</h2>
-      <p>Training Sessions: {{ user.count_of_trainings }}</p>
-      <p>Axes Thrown: {{ user.throw_count }}</p>
-
-      <h3>Hatchet Stats</h3>
-      <p>Hatchets Thrown: {{ user.hatchet_throws.throw_count }}</p>
-      <p>Average Hatchet Score: {{ user.hatchet_throws.average_axe }}</p>
-      <p>Bullseye Attempts: {{ user.hatchet_throws.bullseye_attempts }}</p>
-      <p>Bullseye Accuracy: {{ user.hatchet_throws.bullseye_accuracy }}%</p>
-      <p>Clutch Attempts: {{ user.hatchet_throws.clutch_attempts }}</p>
-      <p>Clutch Accuracy: {{ user.hatchet_throws.clutch_accuracy }}%</p>
-
-      <h3>Big Axe Stats</h3>
-      <p>Big Axes Thrown: {{ user.big_axe_throws.throw_count }}</p>
-      <p>Average Big Axe Score: {{ user.big_axe_throws.average_axe }}</p>
-      <p>Bullseye Attempts: {{ user.big_axe_throws.bullseye_attempts }}</p>
-      <p>Bullseye Accuracy: {{ user.big_axe_throws.bullseye_accuracy }}%</p>
-      <p>Clutch Attempts: {{ user.big_axe_throws.clutch_attempts }}</p>
-      <p>Clutch Accuracy: {{ user.big_axe_throws.clutch_accuracy }}%</p>
+  <div class="UsersShow container text-center">
+    <div class="row justify-content-md-center">
+      <h1>{{ user.name }}</h1>
+      <div v-if="user.count_of_trainings > 0">
+        <p>Sessions: {{ user.count_of_trainings }}</p>
+        <p>Axes Thrown: {{ user.throw_count }}</p>
+        <router-link to="/trainings">My Sessions</router-link>
+      </div>
+      <div v-else>
+        <router-link to="/trainings">Start a Training!</router-link>
+      </div>
     </div>
-    <div v-else>
-      <router-link to="/trainings">Start a Training!</router-link>
-    </div>
+
+    <table v-if="user.count_of_trainings > 0" class="table">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">Hatchet</th>
+          <th scope="col">Big Axe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Axes Thrown</th>
+          <td>{{ user.hatchet_throws.throw_count }}</td>
+          <td>{{ user.big_axe_throws.throw_count }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Average Score</th>
+          <td>{{ user.hatchet_throws.average_axe }}</td>
+          <td>{{ user.big_axe_throws.average_axe }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Bullseye Attempts</th>
+          <td>{{ user.hatchet_throws.bullseye_attempts }}</td>
+          <td>{{ user.big_axe_throws.bullseye_attempts }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Bullseye Accuracy</th>
+          <td>{{ user.hatchet_throws.bullseye_accuracy }}</td>
+          <td>{{ user.big_axe_throws.bullseye_accuracy }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Clutch Attempts</th>
+          <td>{{ user.hatchet_throws.clutch_attempts }}</td>
+          <td>{{ user.big_axe_throws.clutch_attempts }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Clutch Accuracy</th>
+          <td>{{ user.hatchet_throws.clutch_accuracy }}</td>
+          <td>{{ user.big_axe_throws.clutch_accuracy }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
